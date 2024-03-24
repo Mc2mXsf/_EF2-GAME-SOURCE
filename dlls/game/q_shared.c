0cbd4855458b1b312f7c001becf88e0147267161
chrissstrahl <Chrissstrahl@users.noreply.github.com>
2024-03-24 05:50:08 +0100

    Fixed strcpy-param-overlap

--- dlls/game/q_shared.c
+++ dlls/game/q_shared.c
@@ -1314,7 +1314,13 @@ void Info_RemoveKey (char *s, const char *key)
 
 		if (!strcmp (key, pkey) )
 		{
-			strcpy (start, s);	// remove this part
+			//--------------------------------------------------------------
+			// GAMEFIX - strcpy-param-overlap - chrissstrahl
+			//--------------------------------------------------------------
+			//strcpy (start, s);	// remove this part
+			memmove(start, s, strlen(s) + 1);
+
+
 			return;
 		}
 
