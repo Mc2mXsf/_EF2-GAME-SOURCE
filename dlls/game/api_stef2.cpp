9b86396eb7edb77dd7c1a2d907acab6bda514630
chrissstrahl <Chrissstrahl@users.noreply.github.com>
2024-04-02 19:23:03 +0200

    Added file to provide a bridge between the general game code and elite force 2 specific code

---
+++ dlls/game/api_stef2.cpp
@@ -0,0 +1,131 @@
+//--------------------------------------------------------------
+// GAMEFIX - ELITE FORCE 2 - API
+// 
+// Elite Force 2 Specific Functions and classes are made available
+// here in a fashion that the regular gamefix code can be ported
+// to fakk2 and others, with very little effort
+//--------------------------------------------------------------
+
+
+#include "api_stef2.hpp"
+#include "mp_manager.hpp"
+
+
+//--------------------------------------------------------------
+// GAMEFIX - Returns if we are in Singleplayer - chrissstrahl
+//--------------------------------------------------------------
+bool gameFix_inSingleplayer()
+{
+	return !gameFix_inMultiplayer();
+}
+
+//--------------------------------------------------------------
+// GAMEFIX - Returns if we are in Multiplayer - chrissstrahl
+//--------------------------------------------------------------
+bool gameFix_inMultiplayer()
+{
+	if (g_gametype->integer == GT_SINGLE_PLAYER) {
+		return false;
+	}
+	if (!multiplayerManager.inMultiplayer()) {
+		return false;
+	}
+	return true;
+}
+
+//--------------------------------------------------------------
+// GAMEFIX - Returns if given entity(Player) is a spectator - chrissstrahl
+//--------------------------------------------------------------
+bool gamefix_isSpectator_stef2(Entity* ent)
+{
+	if (!ent || !gameFix_inMultiplayer()) {
+		return false;
+	}
+
+	if ( ent->isSubclassOf(Player) && multiplayerManager.isPlayerSpectator((Player*)ent)) {
+		return true;
+	}
+	return false;
+}
+
+//--------------------------------------------------------------
+// GAMEFIX - Returns if given entity(Sentient) is dead - chrissstrahl
+//--------------------------------------------------------------
+bool gameFix_isDead(Entity* ent)
+{
+	if (!ent || ent->isSubclassOf(Sentient) && ent->deadflag) {
+		return true;
+	}
+	return false;
+}
+
+//--------------------------------------------------------------
+// GAMEFIX - Returns if server is dedicated - chrissstrahl
+//--------------------------------------------------------------
+bool gameFix_isDedicatedServer()
+{
+	return dedicated->integer;
+}
+
+//--------------------------------------------------------------
+// GAMEFIX - Returns if this is a Listen Server - chrissstrahl
+//--------------------------------------------------------------
+bool gameFix_isListenServer()
+{
+	return !dedicated->integer;
+}
+
+
+//--------------------------------------------------------------
+// GAMEFIX - Returns if server is running on Windows - chrissstrahl
+//--------------------------------------------------------------
+bool gameFix_isWindowsServer()
+{
+#ifdef WIN32
+	return true;
+#else
+	return false;
+#endif
+}
+
+//--------------------------------------------------------------
+// GAMEFIX - Returns if server is running on Linux - chrissstrahl
+//--------------------------------------------------------------
+bool gameFix_isLinuxServer()
+{
+#ifdef __linux__
+	return true;
+#else
+	return false;
+#endif
+}
+
+//--------------------------------------------------------------
+// GAMEFIX - Returns if player is host - chrissstrahl
+//--------------------------------------------------------------
+bool gameFix_isHost(Player *player)
+{
+#ifdef __linux__
+	return false;
+#endif
+
+	if (g_gametype->integer == GT_SINGLE_PLAYER || g_gametype->integer == GT_BOT_SINGLE_PLAYER) {
+		return true;
+	}
+
+	cvar_t* cl_running = gi.cvar_get("cl_running");
+	if (dedicated->integer == 0 && player->entnum == 0 && (cl_running ? cl_running->integer : 0)) {
+		return true;
+	}
+	return false;
+}
+
+//--------------------------------------------------------------
+// GAMEFIX - Prints Text to player hud/chat - chrissstrahl
+//--------------------------------------------------------------
+void gameFix_hudPrint(Player* player, str sText)
+{
+	if (player && sText && sText.length()) {
+		player->hudPrint(sText.c_str());
+	}
+}
---
+++ dlls/game/api_stef2.hpp
@@ -0,0 +1,21 @@
+//--------------------------------------------------------------
+// GAMEFIX - ELITE FORCE 2 - API
+// 
+// Elite Force 2 Specific Functions and classes are made avialable
+// here in a fashion that the regular gamefix code can be ported
+// to fakk2 and others, with very little effort
+//--------------------------------------------------------------
+
+
+#pragma once
+#include "_pch_cpp.h"
+
+
+bool gameFix_inMultiplayer();
+bool gamefix_isSpectator_stef2(Entity* ent);
+bool gameFix_isDead(Entity* ent);
+bool gameFix_isDedicatedServer();
+bool gameFix_isListenServer();
+bool gameFix_isWindowsServer();
+bool gameFix_isLinuxServer();
+void gameFix_hudPrint(Player* player, str sText);
