3f900ef715d74ecc782fc14baaade97ee2c091ee
chrissstrahl <Chrissstrahl@users.noreply.github.com>
2024-06-17 11:40:32 +0200

    Updated and added strings

--- dlls/game/api_stef2_strings.hpp
+++ dlls/game/api_stef2_strings.hpp
@@ -31,16 +31,18 @@ constexpr auto _GFixEF2_INFO_GAMEFIX_mapCheckFromMaplist = "%s: %s not found in 
 
 constexpr auto _GFixEF2_MSG_FUNC_FinishMissionFailed = "^1=/\\=^3 Mission Failed ^1=/\\=\n";
 constexpr auto _GFixEF2_MSG_FUNC_callvote_singleplayer_not_allowed = "^3Singleplayer^8 levels ^3not allowed^8 on the server!^8 seta gfix_allowSpMaps 1 -> to allow.\n";
-constexpr auto _GFixEF2_MSG_FUNC_callvote_mpMaplist = "cvar mp_useMapList needs to be 1 and mp_mapList needs to be populated\n";
+constexpr auto _GFixEF2_MSG_FUNC_callvote_mpMaplist = "Cvar mp_useMapList needs to be 1 and mp_mapList needs to be populated\n";
 constexpr auto _GFixEF2_MSG_FUNC_callVote_specificVotes = "Server Specific: %s.\n";
 constexpr auto _GFixEF2_MSG_FUNC_callVote_ini_err_cmdEmpty = "callvote.ini error key 'command' has no value!\n";
 constexpr auto _GFixEF2_MSG_FUNC_callVote_cmd_req_arg_range = "$$InvalidVote$$ %s requires argument (%s) range: %s.\n";
-constexpr auto _GFixEF2_MSG_FUNC_callVote_cmd_req_cvar_be = "Vote %s requires cvar^3 %s ^8to be^3 %s^8.\n";
-constexpr auto _GFixEF2_MSG_FUNC_callVote_arg_invalid = "$$InvalidVote$$ Vote Argument for command %s!\n";
+constexpr auto _GFixEF2_MSG_FUNC_callVote_cmd_req_arg_length = "$$InvalidVote$$ %s requires argument (%s) max length: %s.\n";
+constexpr auto _GFixEF2_MSG_FUNC_callVote_cmd_req_cvar_be = "Vote %s requires Cvar^3 %s ^8to be^3 %s^8.\n";
+constexpr auto _GFixEF2_MSG_FUNC_callVote_arg_invalid = "$$InvalidVote$$ Vote Argument for command: %s!\n";
 constexpr auto _GFixEF2_MSG_FUNC_callVote_arg_valid = "Valid: %s.\n";
 constexpr auto _GFixEF2_MSG_FUNC_callVote_willForceReload = "^6WARNING:^3Vote %s ^3will force a level reload!\n";
 constexpr auto _GFixEF2_MSG_FUNC_callVote_changeTakeEffect = "^3Vote changes will take effect after level change.\n";
-constexpr auto _GFixEF2_MSG_FUNC_callVote_exceeded_length = "$$InvalidVote$$ Exceeded given length limit of: %d\n";
+constexpr auto _GFixEF2_MSG_FUNC_callVote_exceeded_length = "$$InvalidVote$$ exceeded given length limit of: %d\n";
+constexpr auto _GFixEF2_MSG_FUNC_callVote_cvarName_exceeds = "Cvar-Name for command %s in callvote.ini exceeded length of: 32\n";
 
 constexpr auto _GFixEF2_WARN_EVENT_ACTOR_attack_FAILED = "Actor::AttackEntity - $%s.attack($entity) failed, entity does not exist\n";
 constexpr auto _GFixEF2_WARN_EVENT_CTHREAD_triggerEntity_FAILED = "triggerEntity($...) - given entity does not exist\n";
