7a05d9b58705d4e06c5e97fe4f178c58939119b9
chrissstrahl <Chrissstrahl@users.noreply.github.com>
2024-07-01 19:04:44 +0200

    restructured how the definitions are listed to easier find functions

--- dlls/game/gamefix.hpp
+++ dlls/game/gamefix.hpp
@@ -85,14 +85,22 @@ struct gamefix_entity_extraData_s
 extern gamefix_entity_extraData_s gamefix_entity_extraData_t[MAX_GENTITIES];
 
 Entity*				gamefix_returnInfoPlayerStart(str info);
+
 Entity*				gamefix_getEntity(str& name);
 Player*				gamefix_getPlayer(int index);
 int					gamefix_getPlayers(bool state);
+bool				gamefix_EntityValid(Entity* entity);
+bool				gamefix_PlayerValid();
+bool				gamefix_checkNotarget(Entity* entity);
+
 Player*				gamefix_getPlayerInsideOfEntity(Entity* eTheBox);
 bool				gamefix_checkEntityInsideOfEntity(Entity* eCheck, Entity* eTheBox);
+
+Player*				gamefix_getPlayerByTargetname(const str& targetname);
 bool				gamefix_targetedByOtherPlayer(Player* player, Entity* entity);
-Player*				gamefix_getActivatorOrClosestPlayer(Entity* entity);
-Player*				gamefix_getActivatorOrClosestPlayerCansee(Actor* actor);
+
+
+
 Player*				gamefix_getClosestPlayer(Entity* entity);
 Player*				gamefix_getClosestPlayerSamePlane(Entity* entity);
 Player*				gamefix_getClosestPlayer(Entity* entity, bool noSpectator, bool noDead, bool samePlane, int planeMaxVertDiff, int planeMaxRange);
@@ -101,30 +109,6 @@ Player*				gamefix_getClosestPlayerActorCanSee(Actor* actor, qboolean useFOV);
 Player*				gamefix_getClosestPlayerCanseeIfNoCurEnemy(Actor* actor);
 Player*				gamefix_getAnyPlayerPreferably();
 Player*				gamefix_getAnyPlayerPreferably(bool noDead, bool noSpectator);
-int					gamefix_getCvarInt(str cvarName);
-float				gamefix_getCvarFloat(str cvarName);
-str					gamefix_getCvar(str cvarName);
-bool				gamefix_EntityValid(Entity* entity);
-bool				gamefix_PlayerValid();
-bool				gamefix_checkNotarget(Entity* entity);
-Entity*				gamefix_getActorFollowTarget(Actor* actor);
-bool				gamefix_actorCanSee(Actor* actor, Entity* entity, qboolean useFOV, qboolean useVisionDistance);
-Entity*				gamefix_actorGetCurrentEnemy(Actor* actor);
-bool				gamefix_actorHates(Actor* actor, Entity* entity);
-bool				gamefix_checkPlayerRanged(Actor* actor, Player* player);
-bool				gamefix_checkPlayerRanged(Actor* actor, bool closestOnly);
-bool				gamefix_checkPlayerUsingWeaponNamed(Player* player, const str& weaponNameOfPlayer);
-Player*				gamefix_getPlayerByTargetname(const str& targetname);
-str					gamefix_getServerLanguage();
-str					gamefix_getLanguage(Player* player);
-qboolean			gamefix_languageEng(const gentity_t* ent);
-qboolean			gamefix_languageDeu(const gentity_t* ent);
-void				gamefix_vstrLocalLanguage(gentity_t* ent);
-str					gamefix_getLocalizedString(Player* player, const str sEnglish,const str sGerman);
-void				gamefix_aiTurnOff();
-void				gamefix_aiTurnOn();
-void				gamefix_printAllClients(const str text);
-void				gamefix_levelFixes();
 void				gamefix_playerSpectator(Player* player);
 void				gamefix_playerChangedTeam(Player* player,const str &realTeamName);
 void				gamefix_playerKilled(Player* player);
@@ -135,47 +119,81 @@ void				gamefix_playerUseItem(Player* player, const char* name);
 void				gamefix_playerScore(Player* player);
 void				gamefix_playerClientBegin(gentity_t* ent);
 void				gamefix_dialogSetupPlayers(Actor* speaker, char localizedDialogName[MAX_QPATH], bool headDisplay);
-str					gamefix_localizeStringForPlayer(Player* player, char unlocal[MAX_QPATH]);
-float				gamefix_dialogGetSoundlength(char sound[MAX_QPATH]);
-void				gamefix_replaceSubstring(char* str, const char* find, const char* replace);
-int					gamefix_findChar(const char* str, const char find);
-int					gamefix_findChars(const char* str, const char* find);
-int					gamefix_findCharsReverse(const char* str, const char* find);
-int					gamefix_findCharsReverse(const char* str, const char* find, int startPos, int endPos);
-int					gamefix_findString(const char* str, const char* find);
-int					gamefix_findStringCase(const str& latinumstack, const str& find,bool wholeWord,int startPos);
-str					gamefix_getStringUntilChar(const str& source, const char& delimiter);
-str					gamefix_getStringUntil(const str &sString, const int iStart, const int iEnd);
-char*				gamefix_getStringUntilChar(const char* source,const char& delimiter);
-int					gamefix_countCharOccurrences(const char* str, const char& ch);
-Entity*				gamefix_spawn(char const* className, char const* model, char const* origin, char const* targetname, const int spawnflags);
-void				gamefix_svFloodProtectDisable();
-void				gamefix_kickBots();
 void				gamefix_playerDelayedServerCommand(int entNum, const char* commandText);
 void				gamefix_playerHandleDelayedServerCommand(void);
 void				gamefix_playerClearDelayedServerCommand(int entNum);
+
+void				gamefix_aiTurnOff();
+void				gamefix_aiTurnOn();
+Entity*				gamefix_getActorFollowTarget(Actor* actor);
+bool				gamefix_actorCanSee(Actor* actor, Entity* entity, qboolean useFOV, qboolean useVisionDistance);
+Entity*				gamefix_actorGetCurrentEnemy(Actor* actor);
+bool				gamefix_actorHates(Actor* actor, Entity* entity);
+bool				gamefix_checkPlayerRanged(Actor* actor, Player* player);
+bool				gamefix_checkPlayerRanged(Actor* actor, bool closestOnly);
+bool				gamefix_checkPlayerUsingWeaponNamed(Player* player, const str& weaponNameOfPlayer);
+Player*				gamefix_getActivatorOrClosestPlayer(Entity* entity);
+Player*				gamefix_getActivatorOrClosestPlayerCansee(Actor* actor);
+
+str					gamefix_getServerLanguage();
+str					gamefix_getLanguage(Player* player);
+qboolean			gamefix_languageEng(const gentity_t* ent);
+qboolean			gamefix_languageDeu(const gentity_t* ent);
+void				gamefix_vstrLocalLanguage(gentity_t* ent);
+str					gamefix_getLocalizedString(Player* player, const str sEnglish,const str sGerman);
+str					gamefix_localizeStringForPlayer(Player* player, char unlocal[MAX_QPATH]);
+void				gamefix_printAllClients(const str text);
+
+int					gamefix_getCvarInt(str cvarName);
+float				gamefix_getCvarFloat(str cvarName);
+str					gamefix_getCvar(str cvarName);
+
+Entity*				gamefix_spawn(char const* className, char const* model, char const* origin, char const* targetname, const int spawnflags);
+void				gamefix_svFloodProtectDisable();
+void				gamefix_kickBots();
+void				gamefix_levelFixes();
+
 bool				gamefix_getFileContents(str sFile, str& contents, bool tokenize);
 bool				gamefix_setFileContents(str sFile, str& contents);
-bool				gamefix_containsNonANSI(const unsigned char* buffer, size_t length);
-char*				gamefix_convertUtf8UmlautsToAnsi(const char* utf8_str);
-char*				gamefix_trimWhitespace(char* input, bool dontTrimNewLine);
-str					gamefix_trimWhitespace(const str& input, bool dontTrimNewLine);
-void				gamefix_listSeperatedItems(Container<str>& container, const str& src, const str& seperator);
-str					gamefix_getExtension(const str& in);
+
 gamefix_iniFileSection* gamefix_iniSectionsParse(const str& file, const char* data, int* section_count);
-void					gamefix_iniSectionsNamesGet(const str& file, Container<str>& sectionList, const str& contents);
+void				gamefix_iniSectionsNamesGet(const str& file, Container<str>& sectionList, const str& contents);
 str					gamefix_iniSectionGet(const str& file, const str& data, const char* section_name);
 str					gamefix_iniSectionSet(const str& file, const str& file_contents, const str& section_name, const str& new_section_contents);
 str					gamefix_iniSectionDelete(const str& file, const str& file_contents, const str& section_name);
 str					gamefix_iniKeyGet(const str& file, const str& section_contents, const str& key, const str& altVal);
 str					gamefix_iniKeySet(const str& file, const str& section_contents, const str& key, const str& value);
+str					gamefix_iniKeyDelete(const str& file, const str& section_contents, const str& key);
 
+bool				gamefix_containsNonANSI(const unsigned char* buffer, size_t length);
+char*				gamefix_convertUtf8UmlautsToAnsi(const char* utf8_str);
+
+str					gamefix_getExtension(const str& in);
 void				gamefix_extractIntegerRange(const str& input, int& first, int& second);
 void				gamefix_extractFloatRange(const str input, float& first, float& second);
+float				gamefix_dialogGetSoundlength(char sound[MAX_QPATH]);
+void				gamefix_listSeperatedItems(Container<str>& container, const str& src, const str& seperator);
+
+int					gamefix_findChar(const char* str, const char find);
+int					gamefix_findChars(const char* str, const char* find);
+int					gamefix_findCharsReverse(const char* str, const char* find);
+int					gamefix_findCharsReverse(const char* str, const char* find, int startPos, int endPos);
+int					gamefix_findString(const char* str, const char* find);
+int					gamefix_findStringCase(const str& latinumstack, const str& find, bool wholeWord, int startPos);
+int					gamefix_countCharOccurrences(const char* str, const char& ch);
+
 char*				gamefix_duplicateString(const char* source);
+void				gamefix_replaceSubstring(char* str, const char* find, const char* replace);
+str					gamefix_getStringUntilChar(const str& source, const char& delimiter);
+char*				gamefix_getStringUntilChar(const char* source, const char& delimiter);
+str					gamefix_getStringUntil(const str& sString, const int iStart, const int iEnd);
+
+char*				gamefix_trimWhitespace(char* input, bool dontTrimNewLine);
+str					gamefix_trimWhitespace(const str& input, bool dontTrimNewLine);
 str					gamefix_cleanMapName(const str& mapname);
 str					gamefix_filterChars(const str filthy, str illegal);
 str					gamefix_stripDoubleChar(const str filthy, str illegal);
+
 void				gamefix_runFrame(int levelTime, int frameTime);
 void				gamefix_shutdownGame();
 void				gamefix_initGame();
